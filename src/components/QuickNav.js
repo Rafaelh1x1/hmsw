aa3523519c23db2fed3de9c8540c2eb6fbaf5553
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-07-24 13:39:26 -0400

    Files reorganized

--- src/components/QuickNav.js
+++ src/components/QuickNav.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { Link, animateScroll as scroll } from "react-scroll";
+
 function QuickNav() {
   return (
     <div>
       <div class="quick-nav">
         <p class="quick-nav-text">HOME</p>
-        <Link
-          to="best-sellers-subheading"
-          smooth={true}
-          duration={500}
-          offset={-50}
-        >
-          <p class="quick-nav-text">BEST-SELLERS</p>
-        </Link>
+
+        <p class="quick-nav-text">BEST-SELLERS</p>
+
         <p class="quick-nav-text">MENU</p>
         <p class="quick-nav-text">LOCATIONS</p>
         <p class="quick-nav-text">ABOUT US</p>
