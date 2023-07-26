86917583e1c8fb5e375b3c11405405ee6cc2a616
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-07-26 18:18:53 -0400

    careers section laid out

--- src/components/careerssection.js
+++ src/components/careerssection.js
@@ -3,14 +3,25 @@ import React from "react";
 export default function CareerSection() {
   return (
     <div className="container">
-      <p> WORK WITH US</p>
-      <div>
-        <p>
-          We’d love to have you join our team! Please use the form below to
-          apply. Feel free to send in your resume even if you don't see the
-          position you're looking for listed.
-        </p>
-        <p>APPLY NOW</p>
+      <div className="careers-section">
+        <div className="careers-img-container">
+          <img
+            src={require("./images/careers-img1.jpg")}
+            alt="Pelicans flying in a group"
+            width="100%vw"
+            height="auto"
+            className="careers-banner-img"
+          />
+          <p className="careers-banner-text">JOIN THE TEAM</p>
+        </div>
+        <div className="careers-description-container">
+          <p className="careers-description-text">
+            We’d love to have you join our team! Please use the form below to
+            apply. Feel free to send in your resume even if you don't see the
+            position you're looking for listed.
+          </p>
+          <p className="apply-now-btn">APPLY NOW</p>
+        </div>
       </div>
     </div>
   );
