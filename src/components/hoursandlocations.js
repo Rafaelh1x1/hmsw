b5ca324a4e7c4b2759b3fb1788d1bfd2d14ebb58
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-07-26 14:14:50 -0400

    Styling for hours and locations section

--- src/components/hoursandlocations.js
+++ src/components/hoursandlocations.js
@@ -2,18 +2,22 @@ import React from "react";
 export default function HoursAndLocations() {
   return (
     <div className="container">
-      <p>HOURS & LOCATIONS</p>
+      <p className="hours-locations-heading">HOURS & LOCATIONS</p>
       <div>
-        <div className="choose-location-container">
-          <p className="choose-location">Temple Terrace</p>
-          <p className="choose-location">Sparkman Wharf</p>
+        <div className="hours-locations-container">
+          <div className="tt-accordion">
+            <p className="tt-hour-location">Temple Terrace</p>
+            <div className="tt-info">
+              <p>11508 N 56th Street, Temple Terrace, FL 33617</p>
+              <p>813-437-3474</p>
+              <p>info@halfmoonseafood.com</p>
+              <p>GET DIRECTIONS</p>
+              <p>Tuesday - Saturday - 11 am - 9 pm</p>
+              <p>Tuesday - Saturday - 11 am - 9 pm</p>
+            </div>
+          </div>
+          <p className="sw-hour-location">Sparkman Wharf</p>
         </div>
-        <p>11508 N 56th Street, Temple Terrace, FL 33617</p>
-        <p>813-437-3474</p>
-        <p>info@halfmoonseafood.com</p>
-        <p>GET DIRECTIONS</p>
-        <p>Tuesday - Saturday - 11 am - 9 pm</p>
-        <p>Tuesday - Saturday - 11 am - 9 pm</p>
       </div>
     </div>
   );
