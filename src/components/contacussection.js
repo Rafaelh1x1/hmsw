959f40da27d9df3d61cc1ebb4d41e897286bafb0
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-07-26 18:33:38 -0400

    Started contact-us section

--- src/components/contacussection.js
+++ src/components/contacussection.js
@@ -3,7 +3,18 @@ import React from "react";
 export default function ContactUsSection() {
   return (
     <div className="container">
-      <p> CONTACT US</p>
+      <div>
+        <div>
+          <img
+            src={require("./images/contact-us-img.jpg")}
+            alt="Lifeguard station on a beach "
+            width="100%vw"
+            height="auto"
+          />
+        </div>
+        <div></div>
+      </div>
+
       <p>
         Send us a message and we’ll get back to you as soon as possible. Looking
         forward to hearing from you.
