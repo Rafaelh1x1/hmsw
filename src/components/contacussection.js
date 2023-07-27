c2657aff4606177cf1f296be026d0929317381f6
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-07-27 12:56:24 -0400

    Contact-us section laid out

--- src/components/contacussection.js
+++ src/components/contacussection.js
@@ -3,27 +3,49 @@ import React from "react";
 export default function ContactUsSection() {
   return (
     <div className="container">
-      <div>
-        <div>
+      <div className="contact-us-section">
+        <div className="contact-us-banner">
           <img
             src={require("./images/contact-us-img.jpg")}
             alt="Lifeguard station on a beach "
             width="100%vw"
             height="auto"
+            className="contact-us-banner-img"
           />
+          <p className="contact-us-banner-text">CONTACT US</p>
         </div>
-        <div></div>
+        <p className="contact-us-description-text">
+          Send us a message and we’ll get back to you as soon as possible.
+          Looking forward to hearing from you.
+        </p>
+        <div className="contact-us-description">
+          <form>
+            <p>
+              <label for="contact-us-name">Name:</label>
+              <br></br>
+              <input type="text" id="conatct-us-name"></input>
+            </p>
+            <p>
+              <label for="contact-us-email">Email:</label> <br></br>
+              <input type="text" id="contact-us-email"></input>
+            </p>
+            <p>
+              <label for="contact-us-phone">Phone Number:</label> <br></br>
+              <input type="text" id="contact-us-phone"></input>
+            </p>
+            <p>
+              <label for="contact-us-reason">Reason for reaching out?</label>{" "}
+              <br></br>
+              <input type="text" id="contact-us-reason"></input>
+            </p>
+            <p>
+              {" "}
+              <label for="contact-us-submit"></label>
+              <input type="submit" id="contact-us-submit"></input>
+            </p>
+          </form>
+        </div>
       </div>
-
-      <p>
-        Send us a message and we’ll get back to you as soon as possible. Looking
-        forward to hearing from you.
-      </p>
-      <p>Name</p>
-      <p>Email</p>
-      <p>Phone Number</p>
-      <p>Reason for reaching out?</p>
-      <p>SEND</p>
     </div>
   );
 }
