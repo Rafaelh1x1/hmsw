3e5f62740ddd647a9d0750a892db1b218f868057
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-08-10 11:25:50 -0400

    styling for the mobile navigation complete

--- src/components/topBanner.js
+++ src/components/topBanner.js
@@ -31,41 +31,43 @@ function TopBanner(props) {
         <p className="top-banner">{props.text}</p>
       </a>
       <div className={`mobile-nav-container ${isMobileView ? "" : "hidden"}`}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          width="24px"
-          height="24px"
-          className={`open-nav ${isNavOpen ? "hidden" : ""}`}
-          onClick={handleNavToggle}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          height="24px"
-          width="24px"
-          className={`close-nav ${isNavOpen ? "" : "hidden"}`}
-          onClick={handleNavToggle}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
+        {isNavOpen ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            height="24px"
+            width="24px"
+            className="nav-button"
+            onClick={handleNavToggle}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            width="24px"
+            height="24px"
+            className="nav-button"
+            onClick={handleNavToggle}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        )}
       </div>
 
       {isNavOpen && <QuickNav />}
