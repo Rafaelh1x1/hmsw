28c09b843a848c65e41c9afd7bd99669a96fea9a
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-08-26 13:20:14 -0400

    Updated Contact Us Section

--- src/components/contacussection.js
+++ src/components/contacussection.js
@@ -17,7 +17,9 @@ export default function ContactUsSection() {
         <p className="contact-us-description-text">
           Send us a message and we’ll get back to you as soon as possible. Feel
           free to send us your resume and information. Looking forward to
-          hearing from you.
+          hearing from you.<br></br>
+          Make sure to send your first and last name, and a way to contact you
+          either phone or email!
         </p>
         <div className="contact-us-description">
           <form
@@ -26,44 +28,12 @@ export default function ContactUsSection() {
             enctype="text/plain"
           >
             <p>
-              <label for="contact-us-name">Name:</label>
-              <br></br>
-              <input type="text" id="conatct-us-name"></input>
-            </p>
-            <p>
-              <label for="contact-us-email">Email:</label> <br></br>
-              <input type="text" id="contact-us-email"></input>
-            </p>
-            <p>
-              <label for="contact-us-phone">Phone Number:</label> <br></br>
-              <input type="text" id="contact-us-phone"></input>
-            </p>
-            <p>
-              <label for="contact-us-reason">Reason for reaching out?</label>{" "}
-              <br></br>
-              <input type="text" id="contact-us-reason"></input>
-            </p>
-            <p>
-              <form
-                action="fileupload.php"
-                enctype="multipart/form-data"
-                method="post"
-              >
-                <label class="custom-uploader" for="file">
-                  Upload A File
-                </label>{" "}
-                <input
-                  id="file"
-                  accept="image/jpeg,image/gif,image/png,application/pdf,image/x-eps"
-                  name="fileToUpload"
-                  type="file"
-                />{" "}
-              </form>
-            </p>
-            <p>
-              {" "}
               <label for="contact-us-submit"></label>
-              <input type="submit" value="send" id="contact-us-submit"></input>
+              <input
+                type="submit"
+                value="Contact Us"
+                id="contact-us-submit"
+              ></input>
             </p>
           </form>
         </div>
