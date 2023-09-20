3188ac38d9502b1d7093767709084a4e84e9248c
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-09-20 08:43:37 -0400

    Better sizings and margins to about us section

--- src/components/topBanner.js
+++ src/components/topBanner.js
@@ -25,48 +25,74 @@ function TopBanner(props) {
   return (
     <div className="top-banner-section load" id="top-banner-section">
       <p className="top-banner">{props.text}</p>
-
-      <div className={`mobile-nav-container ${isMobileView ? "" : "hidden"}`}>
-        {isNavOpen ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            height="24px"
-            width="24px"
-            className="nav-button"
-            onClick={handleNavToggle}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            width="24px"
-            height="24px"
-            className="nav-button"
-            onClick={handleNavToggle}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
-        )}
-      </div>
+      {isMobileView && (
+        <div className={`mobile-nav-container ${isMobileView ? "" : "hidden"}`}>
+          {isNavOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              height="36px"
+              width="36px"
+              className="nav-button"
+              onClick={handleNavToggle}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              width="36px"
+              height="36px"
+              className="nav-button"
+              onClick={handleNavToggle}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          )}
+        </div>
+      )}
 
       {isNavOpen && (
+        <div class="quick-nav">
+          <AnchorLink href="#menu-section">
+            <p class="quick-nav-text" onClick={handleNavToggle}>
+              MENU
+            </p>
+          </AnchorLink>
+          <AnchorLink href="#hours-locations-section">
+            <p class="quick-nav-text" onClick={handleNavToggle}>
+              HOURS & LOCATIONS
+            </p>
+          </AnchorLink>
+          <AnchorLink href="#about-us-section">
+            <p class="quick-nav-text" onClick={handleNavToggle}>
+              ABOUT US
+            </p>
+          </AnchorLink>
+          <AnchorLink href="#contact-us-section">
+            <p class="quick-nav-text" onClick={handleNavToggle}>
+              CONTACT
+            </p>
+          </AnchorLink>
+        </div>
+      )}
+
+      {!isMobileView && (
         <div class="quick-nav">
           <AnchorLink href="#menu-section">
             <p class="quick-nav-text" onClick={handleNavToggle}>
